22af712c963e11539b3b1684b459d578c95abc91
p.slant <p.slant@4e9a9349-2078-a4cd-d3db-e6ffeb2e003b>
2011-06-27 02:45:56 +0000

    Работа с моделью данных

--- surd_gui/trunk/engine.h
+++ surd_gui/trunk/engine.h
@@ -3,24 +3,49 @@
 
 #include <QObject>
 #include <QStandardItemModel>
+#include <QMap>
+
 #include "mytypes.h"
+#include "message.h"
 
 using namespace VPrn;
 
 class Engine : public QObject
-    {
+{
     Q_OBJECT
 public:
     explicit Engine(QObject *parent = 0);
     ~Engine();
 
     QStandardItemModel *model(){return data_model;}
+    QStandardItemModel *prnModel(){return prnInfoModel;}
+public slots:
+    void getUserId();
+    void parseNetworkMessage(const Message & msg);
+    /**
+      * @brief Заполняет модель Юзер!Мандат!ID для принтера с ID = prnID
+      * и добавляет туда список всех пользователей
+      */
+    void getUsersListForPrinter(qint32 prnId);
 
 signals:
     void error(VPrn::AppErrorType errCode,QString error_message);
-
+    void definedAuthData(const QString &login, const QString & mandat);
 private:
+    /**
+      * @fn void updateData();
+      * @brief Обновляет данные в моделях СПИСОК_ПРИНТЕРОВ, СПИСОК_ПОЛЬЗОВАТЕЛЕЙ
+      */
+    void updateData();
+
     QStandardItemModel *data_model;
-    };
+    QStandardItemModel *prnInfoModel;
+    QString m_login;
+    QString m_mandat; // S0-15:C0-1024
+
+    //PrinterInfoList prnList;
+    //UserInfoList    userList;
+    //QMap<qint32,qint32> relPrnUsr; // Таблица связей Принтер->Допущенные поьзователи
+};
 
 #endif // ENGINE_H
