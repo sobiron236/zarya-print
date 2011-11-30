7cabfe764117504e986b404aac18bf9b0bea5b2f
p.slant <p.slant@4e9a9349-2078-a4cd-d3db-e6ffeb2e003b>
2011-11-30 17:35:37 +0000

    Финальное окно мастера

---
+++ trunk/client_prn/trunk/finishpage.cpp
@@ -0,0 +1,5 @@
+#include "finishpage.h"
+
+FinishPage::FinishPage()
+{
+}
---
+++ trunk/client_prn/trunk/finishpage.h
@@ -0,0 +1,12 @@
+#ifndef FINISHPAGE_H
+#define FINISHPAGE_H
+
+#include <QWidget>
+
+class FinishPage : public QWidget
+{
+public:
+    FinishPage();
+};
+
+#endif // FINISHPAGE_H
