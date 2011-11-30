54e44623050f28dfc3f3586784fb021f5aa5dae4
p.slant <p.slant@4e9a9349-2078-a4cd-d3db-e6ffeb2e003b>
2011-11-30 18:07:13 +0000

    Синхронизация

--- trunk/surd_gui/trunk/editprinterproperty.h
+++ trunk/surd_gui/trunk/editprinterproperty.h
@@ -13,8 +13,9 @@ class QSpacerItem;
 class QComboBox;
 class QTreeView;
 class QPushButton;
-class QStandardModel;
+class QStandardItemModel;
 class QModelIndex;
+class QAction;
 QT_END_NAMESPACE
 
 class EditPrinterProperty : public QDialog
@@ -22,10 +23,16 @@ class EditPrinterProperty : public QDialog
     Q_OBJECT
 public:
     explicit EditPrinterProperty(QWidget *parent = 0);
-    void setVisiblePartModel(QStandardModel *model,QModelIndex *root);
+    void setVisiblePartModel(QStandardItemModel *model,QModelIndex &root);
+    void setPrinterAtrib(const QString &name, const QString &mandat);
 signals:
 
 public slots:
+private slots:
+    void addUser();
+    void delUser();
+    void editUser();
+    void showContextMenu(const QPoint &pnt);
 private:
     QLineEdit *printerLineEdit;
     QTreeView *usersList;
@@ -34,6 +41,9 @@ private:
 
     QPushButton *saveButton;
     QPushButton *cancelButton;
+    QAction *m_addAction;
+    QAction *m_delAction;
+    QAction *m_editAction;
 };
 
 #endif // EDITPRINTERPROPERTY_H
